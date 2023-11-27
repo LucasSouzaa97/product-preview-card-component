1c75414453ac22c793548bf38f419a5e998c6999
Lucas de Souza <111311202+LucasSouzaa97@users.noreply.github.com>
2023-11-27 12:04:28 -0300

    Added Montserrat and Fraunces fonts
    Added Montserrat and Fraunces font files to the assets along with their respective license files complying with the SIL Open Font License. Updated main.css to include new @font-face styles. Modified tailwind.config.js to include the new fonts in the theme's fontFamily section, along with adjustments to screen sizes and color themes. These changes enhance website's typography and visual design.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{js}', './**/*.{html}'],
+    content: ['./src/**/*.js', './**/*.html'],
     theme: {
+        screens: {
+            desktop: '1440px',
+        },
+        colors: {
+            dark_cyan: 'hsl(158, 36%, 37%)',
+            cream: 'hsl(30, 38%, 92%)',
+            very_dark_blue: 'hsl(212, 21%, 14%)',
+            dark_grayish_blue: 'hsl(228, 12%, 48%)',
+            white: 'hsl(0, 0%, 100%)',
+        },
+        fontFamily: {
+            montserrat: ['Montserrat', 'sans-serif'],
+            fraunces: ['Fraunces', 'serif'],
+        },
+        fontSize: {
+            body_copy: '14px',
+        },
         extend: {},
     },
     plugins: [],
