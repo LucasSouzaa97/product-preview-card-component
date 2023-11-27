a0befedd5d588e82aeac34930a30001cfc6fdd96
Lucas de Souza <111311202+LucasSouzaa97@users.noreply.github.com>
2023-11-27 10:50:56 -0300

    Update TailwindCSS content configuration
    The content array in the TailwindCSS configuration file has been updated. Previously it was empty, but has now been populated with patterns that match JavaScript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ['./src/**/*.{js}', './**/*.{html}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
